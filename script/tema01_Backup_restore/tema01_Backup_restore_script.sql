a48efbdc175c37e44d5fdca596b89bb8e8b843a8
agostina ortellado <ortelladoagos@gmail.com>
2024-11-08 11:10:58 -0300

    agrego archivos en tema backup y restore

---
+++ script/tema01_Backup_restore/tema01_Backup_restore_script.sql
@@ -0,0 +1,23 @@
+/*Tema: Backup y restore. Backup en línea.
+
+Objetivos de Aprendizaje:
+
+Conocer las técnicas de backup y restore, incluyendo backup en línea.
+Implementar estrategias de respaldo para asegurar la integridad y recuperación de datos.
+Criterios de Evaluación:
+
+Implementación correcta de los procedimientos de backup y restore.
+Documentación del proceso y verificación de la restauración de datos en los puntos indicados.
+Claridad en las conclusiones sobre la efectividad del proceso.
+Tareas: 
+
+Verificar que el modelo de recuperación de la base de datos esté en el modo adecuado para realizar backup en línea
+Realizar un backup full de la base de datos.
+Generar 10 inserts sobre una tabla de referencia.
+Realizar backup del archivo de log y registrar la hora del backup
+Generar otros 10 insert sobre la tabla de referencia.
+Realizar nuevamente backup de archivo de log  en otro archivo físico.
+Restaurar la base de datos al momento del primer backup del archivo de log. Es decir después de los primeros 10 insert.
+Verificar el resultado.
+Restaurar la base de datos aplicando ambos archivos de log.
+Expresar sus conclusiones.*/
